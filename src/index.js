75ae44317e79cfe0165dc129e57322cebe3949f0
KTGK <quyen11b6@gmail.com>
2024-11-19 09:54:25 +0700

    change cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,13 @@ require('dotenv').config();
 const app = express();  
 const PORT = process.env.PORT || 5000;  
 
-app.use(cors());  
+app.use(
+	cors({
+		origin: '*', // Cho phép tất cả các origin trong môi trường dev
+		methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);  
 app.use(express.json());
 // console.log("MongoDB URI:", process.env.MONGODB_URI);
 

46174c9af6caff697a0c2b3b2aacd9dfdb168f93
KTGK <quyen11b6@gmail.com>
2024-11-19 09:27:57 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());  
 app.use(express.json());
-console.log("MongoDB URI:", process.env.MONGODB_URI);
+// console.log("MongoDB URI:", process.env.MONGODB_URI);
 
 const ConnectToMongoDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://nguyendinhquyen:nS7RPbIE4yCJNAaN@quyen.0ukw4.mongodb.net/travel-db?retryWrites=true&w=majority&appName=quyen")
+        const conn = await mongoose.connect("mongodb+srv://nguyendinhquyen:nS7RPbIE4yCJNAaN@quyen.0ukw4.mongodb.net/travel-db?retryWrites=true&w=majority&appName=quyen" || process.env.MONGODB_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log("Error to connect to MongoDB", error.message);
